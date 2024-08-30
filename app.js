2d5093a6527757d56d58235799774df373ff68a4
Badal <surajbadal69@gmail.com>
2024-08-30 13:03:32 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const https = require('https');
 const ejs = require('ejs');
+const dotenv = require('dotenv')
+
+dotenv.config();
 
 const app = express();
 
@@ -10,19 +14,46 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-let inputDate = '';
+let inputDate = "";
 
 app.get('/', function (req, res) {
+  // res.sendFile(__dirname+'/index.html');
+
   const date = new Date();
   const currentDate = date.toJSON().slice(0, 10);
-  res.render("IOD", {
-    picTitle: 'Default Title',
-    image: 'Default Image URL',
-    about: 'Default Explanation',
-    currentDate: currentDate,
+
+  const apikey = process.env.NASA_API_KEY;
+  console.log(apikey);
+  // apiKey=keyFile.config();
+  url = 'https://api.nasa.gov/planetary/apod?api_key=' + apikey + '&date=' + inputDate;
+
+  https.get(url, function (response) {
+    response.on("data", function (data) {
+      const nasaData = JSON.parse(data);
+      const imageUrl = nasaData.hdurl;
+      const title = nasaData.title;
+      const abouImage = nasaData.explanation;
+      res.render("IOD", {
+        picTitle: title,
+        image: imageUrl,
+        about: abouImage,
+        currentDate: currentDate,
+      });
+      // console.log("statusCode- " + response.statusCode);
+    });
+
+
   });
+
+
 });
 
+app.post('/', function (req, res) {
+  inputDate = req.body.date;
+  res.redirect('/');
+});
+
+
 app.listen(3000, function () {
-  console.log('Server started at port no 3000');
+  console.log('Sever started at port no 3000');
 });
